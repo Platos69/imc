eba3b39cf97a8f3ecb75cbfc2f3d0f149175bf72
BigPlatos <139228170+Platos69@users.noreply.github.com>
2024-02-09 11:04:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ buttonSubmit.addEventListener('click', () => {
 
     calculateIMC = weightValue / (heightValue * heightValue)
 
-    if (calculateIMC < 16.9) {
+    if (calculateIMC < 17) {
         showText()
         textIMC.textContent = 'Você está muito abaixo do peso!'
-    } else if (calculateIMC > 16.9 && calculateIMC <= 18.4) {
+    } else if (calculateIMC < 18.5) {
         showText()
         textIMC.textContent = 'Você está no peso normal!'
-    } else if (calculateIMC > 18.4 && calculateIMC <= 24.9) {
+    } else if (calculateIMC < 25) {
         showText()
         textIMC.textContent = 'Você está acima do peso!'
-    } else if (calculateIMC > 24.9 && calculateIMC <= 34.9) {
+    } else if (calculateIMC < 30) {
         showText()
         textIMC.textContent = 'Você está com obesidade grau I!'
-    } else if (calculateIMC > 35 && calculateIMC <= 40) {
+    } else if (calculateIMC < 35) {
         showText()
         textIMC.textContent = 'Você está com obesidade grau II!'
-    } else if (calculateIMC > 40) {
+    } else if (calculateIMC < 40) {
         showText()
         textIMC.textContent = 'Você está com obesidade grau III!'
     }
